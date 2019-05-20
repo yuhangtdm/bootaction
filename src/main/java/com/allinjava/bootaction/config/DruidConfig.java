f757de9771fabcaf5e29dc6b3e833b045267f77f
yuhang <649411629@qq.com>
2019-05-20 13:47:07 +0800

    springboot整合druid并增加监控

---
+++ src/main/java/com/allinjava/bootaction/config/DruidConfig.java
@@ -0,0 +1,86 @@
+package com.allinjava.bootaction.config;
+
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.wall.WallConfig;
+import com.alibaba.druid.wall.WallFilter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: yuhang
+ * @date: 2018/9/1
+ */
+@Configuration
+public class DruidConfig {
+
+    /**
+     * 配置druid数据源
+     */
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource druid(WallFilter wallFilter){
+        DruidDataSource dataSource= new DruidDataSource();
+        List<Filter> filters = new ArrayList<>();
+        filters.add(wallFilter);
+        dataSource.setProxyFilters(filters);
+        return dataSource;
+    }
+
+    /**
+     * 配置durid监控平台
+     * 访问 http://localhost:8801/bootAction/druid/index.html 即可
+     */
+    @Bean
+    public ServletRegistrationBean statViewServlet(){
+        //创建servlet注册实体
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
+        //设置ip白名单
+        servletRegistrationBean.addInitParameter("allow","127.0.0.1");
+        //设置ip黑名单，如果allow与deny共同存在时,deny优先于allow
+        servletRegistrationBean.addInitParameter("deny","192.168.0.19");
+        //设置控制台管理用户
+        servletRegistrationBean.addInitParameter("loginUsername","druid");
+        servletRegistrationBean.addInitParameter("loginPassword","123456");
+        //是否可以重置数据
+        servletRegistrationBean.addInitParameter("resetEnable","false");
+        return servletRegistrationBean;
+    }
+    @Bean
+    public FilterRegistrationBean statFilter(){
+        //创建过滤器
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new WebStatFilter());
+        //设置过滤器过滤路径
+        filterRegistrationBean.addUrlPatterns("/*");
+        //忽略过滤的形式
+        filterRegistrationBean.addInitParameter("exclusions","*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+
+    /**
+     * 配置druid可以一次执行多条sql
+     */
+    @Bean(name = "wallConfig")
+    WallConfig wallFilterConfig(){
+        WallConfig wc = new WallConfig ();
+        wc.setMultiStatementAllow(true);
+        return wc;
+    }
+    @Bean(name = "wallFilter")
+    @DependsOn("wallConfig")
+    WallFilter wallFilter(WallConfig wallConfig){
+        WallFilter wfilter = new WallFilter ();
+        wfilter.setConfig(wallConfig);
+        return wfilter;
+    }
+}
